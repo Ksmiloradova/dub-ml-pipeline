24c8e22860e26013edd252db1ff887257c1f1fcc
Slava Cado <little.cado1@gmail.com>
2023-10-22 14:34:14 +0300

    Improve error handling

--- gender_detection.py
+++ gender_detection.py
@@ -7,7 +7,21 @@ headers = {"Authorization": f"Bearer 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"}
 # Returns the voice gender ('female' or 'male'), str type
 
 def voice_gender_detection(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
+    try:
+        with open(filename, "rb") as f:
+            data = f.read()
+    except FileNotFoundError:
+        print(f"Error: {filename} not found.")
+        return None
+
     response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()[0]['label']
+
+    if response.status_code == 200:
+        try:
+            return response.json()[0]['label']
+        except (KeyError, IndexError, TypeError):
+            print("Error: Unexpected API response format.")
+            return None
+    else:
+        print(f"API Error {response.status_code}: {response.text}")
+        return None
--- text_to_speech.py
+++ text_to_speech.py
@@ -1,20 +1,19 @@
 from elevenlabs import generate
 
-def text_to_speech(text, detected_gender, is_video, video_path=None):
-    if detected_gender == 'female':
-        audio = generate(
-            text=text,
-            voice="Rachel",
-            model="eleven_multilingual_v2"
-        )
-    else:
-        audio = generate(
-            text=text,
-            voice="Josh",
-            model="eleven_multilingual_v2"
-        )
+VOICE_MAPPING = {
+    "female": "Rachel",
+    "male": "Josh"
+}
 
-        # if is_video:
+def text_to_speech(text, detected_gender, is_video, video_path=None):
+    voice = VOICE_MAPPING.get(detected_gender, "Josh")  # Default to "Josh" if gender is not recognized
+    audio = generate(
+        text=text,
+        voice=voice,
+        model="eleven_multilingual_v2"
+    )
+
+    # if is_video:
 
     with open('new_audio.mp3', mode='bx') as f:
         f.write(audio)
